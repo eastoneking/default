ea5ae0ba64f74401d73c93816de771f10e9314d8
wangds <wangds@gmail.com>
2014-01-14 21:25:07 +0800

    补充

---
+++ eastone-common/src/main/java/eastone/common/delegate/Delegant.java
@@ -0,0 +1,15 @@
+/*
+ * Delegant.java
+ * eastone.common.delegate.Delegant
+ * 2014-01-12 19:37
+ */
+package eastone.common.delegate;
+
+/**
+ * 委托者.
+ * @author WANGDS
+ *
+ */
+public interface Delegant {
+
+}
